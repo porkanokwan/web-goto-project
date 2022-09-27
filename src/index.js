d014e9cebe4a772cb21ddcc97494fe62c20c2883
porkanokwan <kanokwan.duly@gmail.com>
2022-09-27 19:59:35 +0700

    fixed index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/js/dist/offcanvas";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./index.css";
 import App from "./App";
