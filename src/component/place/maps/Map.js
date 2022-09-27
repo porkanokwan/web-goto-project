d394fab6f2b45815f84501d02228aa71860b0398
porkanokwan <kanokwan.duly@gmail.com>
2022-09-27 22:29:26 +0700

    add error in map

--- src/component/place/maps/Map.js
+++ src/component/place/maps/Map.js
@@ -2,12 +2,18 @@ import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import SpinnerGrow from "../../common/SpinnerGrow";
 
 function Map({ lat, lng, category }) {
-  const { isLoaded } = useLoadScript({
+  const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_API_KEY_MAP,
   });
   const center = { lat, lng };
 
   if (!isLoaded) return <SpinnerGrow />;
+  if (loadError)
+    return (
+      <div class="alert alert-danger mt-2 mx-5 fs-4 text-center" role="alert">
+        Map cannot be loaded right now, sorry.
+      </div>
+    );
   return (
     <>
       <GoogleMap
